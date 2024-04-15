e89e05d4977bbbacb123e5508440a1f62e69f0a0
jiro_chan03 <153165565+jirochan0324@users.noreply.github.com>
2024-04-15 14:32:19 +0900

    up

---
+++ js/slide.js
@@ -0,0 +1,35 @@
+$(function(){
+  $('.slides').slick({
+    autoplay: true, // 自動でスクロール
+    autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+    speed: 7000, // スライドが流れる速度を設定
+    cssEase: "linear", // スライドの流れ方を等速に設定
+    slidesToShow: 5, // 表示するスライドの数
+    infinite: true,
+    slidesToScroll: 1,
+    swipe: false, // 操作による切り替えはさせない
+    arrows: false, // 矢印非表示
+    pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+    pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+});
+});
+
+
+
+
+$(function(){
+  $('.hslides').slick({
+    autoplay: true, // 自動でスクロール
+    autoplaySpeed: 0, // 自動再生のスライド切り替えまでの時間を設定
+    speed: 7000, // スライドが流れる速度を設定
+    cssEase: "linear", // スライドの流れ方を等速に設定
+    slidesToShow: 4, // 表示するスライドの数
+    infinite: true,
+    slidesToScroll: 1,
+    swipe: false, // 操作による切り替えはさせない
+    arrows: false, // 矢印非表示
+    pauseOnFocus: false, // スライダーをフォーカスした時にスライドを停止させるか
+    pauseOnHover: false, // スライダーにマウスホバーした時にスライドを停止させるか
+});
+});
+
